25410c0608775646f781ed49b8edbc76b6000245
Pratoosh garg <144068212+Pratoosh-18@users.noreply.github.com>
2023-10-06 10:01:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const banner = document.querySelector("#banner")
 const footer = document.querySelector("#footer")
 const contact = document.querySelector("#contact")
 const workCard = document.querySelector("#workCard")
-const service = document.querySelector("#service")
+const allheader = document.querySelector("#allheader")
 
 console.log(menu)
 Array.from(menu).forEach((item) => {
@@ -31,6 +31,15 @@ Array.from(menu).forEach((item) => {
     })
 })
 
+var a = document.getElementById("1");
+var b = document.getElementById("2");
+var c = document.getElementById("3");
+var d = document.getElementById("4");
+var e = document.getElementById("5");
+var f = document.getElementById("6");
+var g = document.getElementById("7");
+var h = document.getElementById("8");
+
 mode.addEventListener("click", () => {
     if (document.body.style.backgroundColor === "white") {
         document.body.style.backgroundColor = "black"
@@ -39,12 +48,29 @@ mode.addEventListener("click", () => {
         footer.style.backgroundColor = "black"
         contact.style.backgroundColor = "white"
         workCard.style.color = "black"
-        service.style.color = "white"
+        allheader.style.backgroundColor = "#272727"
+        a.style.color = "white";
+        b.style.color = "white";
+        c.style.color = "white";
+        d.style.color = "white";
+        e.style.color = "white";
+        f.style.color = "white";
+        g.style.color = "white";
+        h.style.color = "white";
 
     } else {
         document.body.style.backgroundColor = "white"
         document.body.style.color = "black"
         banner.style.backgroundColor = "#f2f5f8"
         footer.style.backgroundColor = "#f2f5f8"
+        allheader.style.backgroundColor = "white"
+        a.style.color = "#555555";
+        b.style.color = "#555555";
+        c.style.color = "#555555";
+        d.style.color = "#555555";
+        e.style.color = "#555555";
+        f.style.color = "#555555";
+        g.style.color = "#555555";
+        h.style.color = "#555555";
     }
 })
