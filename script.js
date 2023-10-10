31c085187b8e33c720b2257a55e0f7b1e25cbaee
Pratoosh garg <144068212+Pratoosh-18@users.noreply.github.com>
2023-10-10 18:10:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ const allheader = document.querySelector("#allheader")
 const serviceCardtext = document.querySelectorAll(".services-stats-card-text")
 const listItem = document.querySelectorAll(".list-item")
 const serviceCard = document.querySelectorAll(".services-stats-card")
+const getstarted = document.querySelector("#getstarted")
+const ourprojects = document.querySelector("#ourprojects")
+const nav = document.querySelector("#nav")
+const blogs = document.querySelector("#blogs")
+const logostyle = document.querySelector("#logostyle")
 
 console.log(menu)
 Array.from(menu).forEach((item) => {
@@ -34,32 +39,37 @@ Array.from(menu).forEach((item) => {
     })
 })
 
-var a = document.getElementById("1");
-var b = document.getElementById("2");
-var c = document.getElementById("3");
-var d = document.getElementById("4");
-var e = document.getElementById("5");
-var f = document.getElementById("6");
-var g = document.getElementById("7");
-var h = document.getElementById("8");
+getstarted.addEventListener("click", () => {
+    document.getElementById('aboutBox').scrollIntoView();
+})
+ourprojects.addEventListener("click", () => {
+    document.getElementById('portfolioBox').scrollIntoView();
+})
 
 mode.addEventListener("click", () => {
     if (document.body.style.backgroundColor === "white") {
         document.body.style.backgroundColor = "black"
         document.body.style.color = "white"
         banner.style.backgroundColor = "black"
-        footer.style.backgroundColor = "black"
+        footer.style.backgroundColor = "#2B3035"
+        footer.style.color = "white"
         contact.style.backgroundColor = "white"
         workCard.style.color = "black"
-        allheader.style.backgroundColor = "#272727"
-        for(let i=0;i<8;i++){
-            listItem[i].style.color="white"
+        blogs.style.color = "white"
+        allheader.style.backgroundColor = "#2B3035"
+        logostyle.innerHTML = `<img src="logowhite.png" height="50px">`
+        mode.innerHTML = "Light Mode"
+        mode.style.color = "black"
+        mode.style.backgroundColor = "white"
+        nav.setAttribute("data-bs-theme", "dark");
+        for (let i = 0; i < 8; i++) {
+            listItem[i].style.color = "white"
         }
-        for(let i=0;i<10;i++){
-            serviceCardtext[i].style.color="white"
+        for (let i = 0; i < 10; i++) {
+            serviceCardtext[i].style.color = "white"
         }
-        for(let i=0;i<10;i++){
-            serviceCard[i].style.boxShadow="0 4px 8px 0 rgb(255, 255, 255, 0.2), 0 6px 20px 0 rgb(255, 255, 255, 0.19)"
+        for (let i = 0; i < 10; i++) {
+            serviceCard[i].style.boxShadow = "0 4px 8px 0 rgb(255, 255, 255, 0.2), 0 6px 20px 0 rgb(255, 255, 255, 0.19)"
         }
 
     } else {
@@ -67,15 +77,23 @@ mode.addEventListener("click", () => {
         document.body.style.color = "black"
         banner.style.backgroundColor = "#f2f5f8"
         footer.style.backgroundColor = "#f2f5f8"
+        footer.style.color = "black"
         allheader.style.backgroundColor = "white"
-        for(let i=0;i<8;i++){
-            listItem[i].style.color="#555555"
+        blogs.style.color = "#555555"
+        logostyle.innerHTML = `<img src="logo.png" height="50px">`
+        mode.innerHTML = "Dark Mode"
+        mode.style.color = "white"
+        mode.style.backgroundColor = "black"
+        nav.removeAttribute("data-bs-theme");
+        for (let i = 0; i < 8; i++) {
+            listItem[i].style.color = "#555555"
         }
-        for(let i=0;i<10;i++){
-            serviceCardtext[i].style.color="#444444"
+        for (let i = 0; i < 10; i++) {
+            serviceCardtext[i].style.color = "#444444"
         }
-        for(let i=0;i<10;i++){
-            serviceCard[i].style.boxShadow="0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+        for (let i = 0; i < 10; i++) {
+            serviceCard[i].style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
         }
+
     }
 })
